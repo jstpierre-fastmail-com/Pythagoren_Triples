af2b6742916f60c0315f5452992fa137d254c783
jstpierre-fastmail-com <81194838+jstpierre-fastmail-com@users.noreply.github.com>
2021-05-16 17:36:57 -0400

    Update Pythagorean_Triples.py

--- Pythagorean_Triples.py
+++ Pythagorean_Triples.py
@@ -5,15 +5,19 @@ import math
 def get_pythagorean_triples(n):
     output = ''
     num_list = []
-    for i in range(1, n + 1):
-        for j in range(1, round(i * (2 * i / 3))):
-            if math.sqrt(i ** 2 + j ** 2) == round(math.sqrt(i ** 2 + j ** 2)):
+
+    max_i = range(1, n + 1)
+    for i in max_i:
+        max_j = range(1, round(i * (2 * i / 3))
+        for j in max_j:
+            c_length = math.sqrt(i ** 2 + j ** 2)
+            if c_length == round(c_length):
                 if str(i) + ',' + str(j) in num_list or str(j) + ',' + str(i) in num_list:
                     continue
                 else:
-                    output += (str(i) + '^2 + ' + str(j) + '^2 = ' + str(
-                        round(math.sqrt(i ** 2 + j ** 2))) + '^2' + "\n")
-                    for k in range(1, round(n / i) + 1):
+                    output += (str(i) + '^2 + ' + str(j) + '^2 = ' + str(c_length) + '^2' + "\n")
+                    max_k = range(1, round(n / i) + 1)
+                    for k in max_k:
                         num_list.append(str(j * k) + ',' + str(i * k))
     return output
 
